3507965af75e9a00eced4c1caad151a981886ab5
Yen Lin <e19970722@gmail.com>
2023-11-19 12:58:36 +0800

    Initial Commit

---
+++ 20231119_MoneyNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20231119_MoneyNote
+//
+//  Created by Yen Lin on 2023/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
